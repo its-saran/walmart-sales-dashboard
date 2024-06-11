3cc5a5edeeee00d792ed815c3d2e8ea4cac31ba3
saran <its.saran.pv@gmail.com>
2024-06-11 18:53:59 +0530

    Inject css inside markdown

--- app.py
+++ app.py
@@ -2,6 +2,14 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
+remove_branding = """
+<style>
+#MainMenu {visibility: hidden;}
+</style>
+"""
+
+st.markdown(remove_branding, unsafe_allow_html=True)
+
 df = pd.read_csv('dataset/WalmartSalesData.csv')
 
 fig = px.bar(df, x='City', y='Revenue', title='Total Sales by City')
